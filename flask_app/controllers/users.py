8baade4f9d41cec761af36f9fed70ab1110dd6ad
Paul De Ulloa <pauld143@gmail.com>
2023-08-15 23:23:48 -0700

    login and registration practice

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,97 @@
+from flask_app import app
+from flask import flash, redirect, render_template, request, session
+from flask_app.models.user import User
+from flask_app import bcrypt
+
+
+@app.route("/")
+def index():
+    """Display the login and registration forms."""
+
+    return render_template("index.html")
+
+
+@app.post("/register")
+def register_user():
+    """Process the registration form."""
+    # validate the form first
+
+    # if invalid, redirect user back to form
+    if not User.registration_is_valid(request.form):
+        return redirect("/")
+    # look for user by email on form
+    potential_user = User.get_by_email(request.form["email"])
+    # if there is a user, redirect to form
+    if potential_user:
+        flash("Email in use. Please log in.", "register")
+        return redirect("/")
+    print("Users not found, okay to register.")
+
+    # hash the user's password (enctypt)
+    hashed_pw = bcrypt.generate_password_hash(request.form["password"])
+    data = {
+        "first_name": request.form["first_name"],
+        "last_name": request.form["last_name"],
+        "email": request.form["email"],
+        "password": hashed_pw,
+    }
+    # save the user to the database
+    user_id = User.create(data)
+    # put the user's id into session
+    session["user_id"] = user_id
+    # message for the users when they successfully register
+    flash("Thank you for registering, you're AWESOME!", "register")
+
+    # redirect to the dashboard
+    return redirect("/dashboard")
+
+
+# THIS IS FOR THE LOGIN BELOW
+@app.post("/login")
+def login():
+    """Processes the LOGIN FORM."""
+    # validate FORM first
+    if not User.login_is_valid(request.form):
+        return redirect("/")
+    # check if user exists by email
+    potential_user = User.get_by_email(request.form["email"])
+    # if they dont exist, flash please register and redirect
+    if not potential_user:
+        flash("Invalid credentials.", "login")
+        return redirect("/")
+    # check password if they exist
+    user = potential_user
+
+    # if password is wrong, flash incorrect and redirect
+    if not bcrypt.check_password_hash(user.password, request.form["password"]):
+        flash("Invalid credentials.", "login")
+        return redirect("/")
+
+    # put the user's id into session
+    session["user_id"] = user.id
+
+    # redirect to the dashboard
+    flash("Thank you for logging in.", "login")
+    return redirect("/dashboard")
+
+
+@app.get("/dashboard")
+def dashboard():
+    """Displays the dashboard template"""
+
+    if "user_id" not in session:
+        flash("Please login below", "login")
+        return redirect("/")
+
+    user = User.get_by_user_id(session["user_id"])
+
+    return render_template("dashboard.html", user=user)
+
+
+@app.get("/logout")
+def logout():
+    """Clears session."""
+
+    session.clear()
+    flash("You have successfully been logged out.", "login")
+    return redirect("/")
